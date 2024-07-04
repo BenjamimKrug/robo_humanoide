8b549828139fdce4c966b990b94429958b94284a
Benjamim Krug <70476830+BenjamimKrug@users.noreply.github.com>
2024-07-04 19:01:00 -0300

    [UPD] script de criação do arquivo dos arrays de compensação

--- códigos de teste básicos/servo_compensated_control/make_compensation_arrays.py
+++ códigos de teste básicos/servo_compensated_control/make_compensation_arrays.py
@@ -4,6 +4,11 @@ import sys
 file_outputs = []
 
 results = []
+try:
+    var_type = sys.argv[3]
+except:
+    var_type = "pwm_val_t"
+
 
 with open(sys.argv[1]) as csvfile:
     reader = csv.reader(csvfile)
@@ -11,7 +16,7 @@ with open(sys.argv[1]) as csvfile:
         results.append(row)
 
 for i in range(1, len(results[0])):
-    file_outputs.append("pwm_val_t " + results[0][i] + "_comp_array[] = {\n")
+    file_outputs.append(var_type + " " + results[0][i] + "_comp_array[] = {\n")
 
 last_pos = len(results) - 1
 
@@ -34,8 +39,17 @@ for j in range(1, len(results[last_pos])):
 for i in range(0, len(results[0]) - 1):
      file_outputs[i] += "};"
 
-print(file_outputs)
+header_guard = sys.argv[2].upper().replace(".", "_")
+last_index = header_guard.rfind("/")
+if last_index != -1:
+    header_guard = header_guard[last_index + 1:]
 
-output = "\n\n".join(file_outputs)
-print(output)
+output = "#ifndef " + header_guard + "\n"
+output += "#define " + header_guard + "\n"
+output += "#include <stdint.h>\n"
+output += "#include <stdio.h>\n\n"
+output += "\n\n".join(file_outputs)
+output += "\n\n#endif"
 
+with open(sys.argv[2], "w") as file:
+    file.write(output)
